90127d13648e68b66bab4ac36759d8b39e09af3a
yesirStrawberry <135397429+yesirStrawberry@users.noreply.github.com>
2026-05-03 17:08:44 +0900

    Create N_Queen.c

---
+++ Do it 자료구조와 함께하는 알고리즘 입문 c언어편/recursive/N_Queen.c
@@ -0,0 +1,65 @@
+/*
+N-Queen 을 recursive하게 구현하라
+*/
+
+#include <stdio.h>
+
+int pos[20];
+int flag_a[20];
+int flag_b[2*(20) - 1]; // / 대각선 i + j
+int flag_c[2*(20) - 1]; // \ 대각선 (N - 1) - i + j
+int cnt = 0; 
+int N; 
+
+void init_flag_pos();
+
+void print_pos();
+
+void set(int i); 
+
+int main(void){
+    printf("N : "); scanf("%d", &N); 
+    init_flag_pos(); 
+    set(0); 
+    printf("cnt : %d", cnt);
+    
+    return 0; 
+}
+
+
+void init_flag_pos(){
+    for(int i = 0; i < N; i++){
+        pos[i] = 0; 
+        flag_a[i] = 0; 
+    } 
+    for(int i = 0; i < N; i++){
+        flag_b[i] = 0; 
+        flag_c[i] = 0;
+    }    
+}
+
+
+void set(int i){
+    for(int j = 0; j < N; j++){
+        if(!flag_a[j] && !flag_b[i+j] && !flag_c[(N - 1)-i+j]){
+            pos[i] = j; 
+            if(i == N - 1) {
+                print_pos();
+                cnt++;
+            }
+            else{
+                flag_a[j] = flag_b[i + j] = flag_c[(N - 1) - i + j] = 1; 
+                set(i + 1);
+                flag_a[j] = flag_b[i + j] = flag_c[(N - 1) - i + j] = 0; 
+            }
+        }
+    }
+}
+
+
+void print_pos(){
+    for(int* p = pos; p < pos + N; p++){
+        printf("% 2d",*p);
+    }
+    putchar('\n');
+}
