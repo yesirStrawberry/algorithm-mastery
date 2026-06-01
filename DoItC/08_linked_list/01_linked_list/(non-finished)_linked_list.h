74a245452e9df94066f2ba012f040808307b5735
yesirStrawberry <135397429+yesirStrawberry@users.noreply.github.com>
2026-06-01 22:23:38 +0900

    Refactor linked list header with function prototypes

--- DoItC/08_linked_list/01_linked_list/(non-finished)_linked_list.h
+++ DoItC/08_linked_list/01_linked_list/(non-finished)_linked_list.h
@@ -4,12 +4,6 @@
 #include "member.h"
 
 
-/* header for source
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-*/
-
 typedef struct __node{
     member data; 
     struct __node *next; 
@@ -20,176 +14,26 @@ typedef struct{
     Node *crnt;
 }List;
 
-static Node* alloc_node(void){
-    Node *new_node = calloc(1, sizeof(Node)); 
-    if (new_node == NULL) {
-        // 유언...
-        fprintf(stderr, "[CRITICAL ERROR] Failed to allocate memory in alloc_node().\n");
-        return NULL;
-    }
-    
-    return new_node; 
-}
+void init_list(List *list);
 
-static void set_node(Node *n, const member *x, const Node *next){
-    if(n == NULL) return; 
-    if(x != NULL) n->data = *x; 
-    n->next = next; 
-    return; 
-}
+Node* search(List *list, const member* x, int compare(const member *x, const member *y));
 
-void init_list(List list){
-    if(list == NULL) return; 
-    list->crnt = NULL; 
-    list->head = NULL; 
-}
+bool insert_front(List *list, const member *x);
 
-Node* search(List *list, const member* x, int compare(const member *x, const member *y)){
-    if(list->head == NULL) return NULL;
-    Node *ptr = list->head; 
-    while(ptr != NULL){
-        if(compare(&(ptr->date), x) == 0){
-            list->crnt = ptr; 
-            return ptr; 
-        }
-        ptr = ptr->next; 
-    }
-    return NULL;
-}
+void insert_reer(List *list, const member *x);
 
-bool insert_front(List *list, const member *x){
-    if(list == NULL || x == NULL) return false; 
-    node *ptr = list->head; 
-    Node *new_node = alloc_node();
-    list->head = new_node; 
-    list->crnt = new_node; 
-    set_node(new_node, x, ptr); 
-    return true; 
-}
+bool remove_front(List *list);
 
-void insert_reer(List *list, const member *x){
-    if(list->head == NULL){
-        insert_front(list, x); 
-        return; 
-    }
-    
-    Node *ptr = list->head; 
-    while(ptr->next != NULL){
-        ptr = ptr->next; 
-    }
-    
-    ptr->next = list->crnt = alloc_node();
-    set_node(ptr->next, x, NULL); 
-}
+void remove_reer(List *list);
 
-bool remove_front(List *list){
-    if(list == NULL || list->head == NULL) return false; 
+void remove_current(List *list);
 
-    Node *tmp = list->head; 
-    list->head = list->crnt = list->head->next; 
+void clear(List *list);
 
-    free(tmp);
-    return true; 
-}
+void print_node(const List *list);
 
-void remove_reer(List *list){
-    if(list->head == NULL) return; 
-    if(list->head->next == NULL){
-        remove_front(list);
-        return; 
-    }
-    Node *ptr = list->head; 
-    Node *pre; 
-    
-    while(ptr->next != NULL){
-        pre = ptr; 
-        ptr = ptr->next; 
-    }
-    set_node(pre, pre->data, NULL);
-    list->crnt = pre; 
-    free(ptr); 
-}
+void print_list(const List *list);
 
-void remove_current(List *list){
-    if(list->head == NULL) return; 
-    if(list->head->next == NULL) {
-        remove_front(list);
-        return;
-    } 
-    
-    // find pre
-    Node *ptr = list->head;
-    Node *pre;
-    while(ptr != list->crnt){
-        pre = ptr; 
-        ptr = ptr->next; 
-    }
-    
-    // cut current node; 
-    set_node(pre, pre->data, ptr->next); 
-    list->crnt = pre; 
-    free(ptr); 
-}
+void terminate_list(List *list); 
 
-void clear(List *list){
-    if(list == NULL) return; 
-
-    Node *curr = list->head; 
-    while(curr != NULL){
-        node *next_curr = curr->next; 
-        free(curr); 
-        curr = next_curr; 
-    }
-    list->head = NULL: 
-    list->crnt = NULL; 
-}
-
-void print_node(const List *list){
-    if(list->crnt == NULL) return; 
-    print_member(&list->crnt->data);
-    return;
-}
-
-void print_list(const List *list){
-    if(list->head == NULL) return; 
-    node *ptr = list->head; 
-    while(ptr->next != NULL){
-        print_member(&pre->data); 
-        ptr = ptr->next; 
-    }
-    return; 
-}
-
-void terminate_list(List *list){
-    clear(list); 
-    return; 
-}
-
-#endif /*__linked_list*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#endif
