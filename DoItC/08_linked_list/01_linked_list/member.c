16b317166beedca2935a960e81f795407bddecab
yesirStrawberry <135397429+yesirStrawberry@users.noreply.github.com>
2026-06-01 22:44:29 +0900

    Add print_member function to display member info

---
+++ DoItC/08_linked_list/01_linked_list/member.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <stdio.h>
+#include "member.h"
+
+void print_member(const member *x){
+    puts("--------------"); 
+    printf("name : %s\n", x->name); 
+    printf("age  : %s\n", x->age); 
+}
