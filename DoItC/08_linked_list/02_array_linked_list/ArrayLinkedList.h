2783ed0e2d30f734a8a5d80b2824f8cf6968b53f
yesirStrawberry <135397429+yesirStrawberry@users.noreply.github.com>
2026-06-06 16:20:25 +0900

    Add ArrayLinkedList header file with function declarations

---
+++ DoItC/08_linked_list/02_array_linked_list/ArrayLinkedList.h
@@ -0,0 +1,46 @@
+#ifndef __ArrayLinkedList
+#define __ArrayLinkedList
+
+#include "member.h"
+
+#define null -1
+
+typedef int Index; 
+
+typedef struct{
+    member data; 
+    Index next; 
+    Index Dnext; 
+}Node; 
+
+typedef struct{
+    Node *arr; 
+    Index head; 
+    Index crnt; 
+    Index max; 
+    Index deleted; 
+}List;
+
+void init_list(List *list, Index size);
+
+Index search(List *list, const member *x, int compr(const member *x, const member *y));
+
+void insert_front(List *list, const member *x);
+
+void insert_rear(List *list, const member *x);
+
+void remove_front(List *list);
+
+void remove_rear(List *list); 
+
+void remove_current(List *list);
+
+void clear(List *list); 
+
+void print_node(const List *list);
+
+void print_list(const List *list);
+
+void terminate_list(List *list); 
+
+#endif /*__ArrayLinkedList*/
