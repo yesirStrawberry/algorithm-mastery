ee7eb5ca88922404541bd53d53e40588feea6cce
Lee Yesung <135397429+yesirStrawberry@users.noreply.github.com>
2026-08-25 19:20:15 +0900

    균형잡힌세상

---
+++ 10weeksALG/2week/O 4949.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main(void){
+  ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); 
+  
+  string s; 
+  while(true){
+    getline(cin, s);
+    cin.ignore(); 
+    if(s == ".") break; 
+
+    stack<char> st;  
+    for(char c : s){
+      if(c != '[' && c != ']' && c != '(' && c != ')') continue; 
+      if(st.size() == 0) st.push(c); 
+      else if(st.top() == '[' && c == ']') st.pop(); 
+      else if(st.top() == '(' && c == ')') st.pop(); 
+      else st.push(c); 
+    }
+    if(st.size() == 0) cout << "yes" << '\n'; 
+    else cout << "no" << '\n'; 
+  } 
+
+  return 0; 
+}
